6bd4414a99b2895123627cfe3ad0d426385eb748
Ashish Gaikwad <ash.gkwd@gmail.com>
2014-10-23 11:22:46 +0530

    SJF added. FCFS modified and renamed to non_preemptive. Its new generic for FCFS and SJF

--- Operating System/cpu scheduling.cc
+++ Operating System/cpu scheduling.cc
@@ -6,6 +6,7 @@
 
 #include <cstdio>
 #include <string>
+#include <algorithm>
 #include <vector>
 
 class Process {
@@ -19,18 +20,8 @@ public:
   int arrival, burst, start, finish, tat, waiting;
 };
 
-int main () {
-  std::vector<Process> v;
-  int N, a, b, total_waiting = 0, total_tat = 0;
-  char s[100];
-
-  printf("Enter total number of processes\n");
-  scanf("%d", &N);
-  printf("Enter Process Name, Arrival Time, Burst Time\n");
-  for (int i = 0; i<N; i++) {
-    scanf("%s%d%d", s, &a, &b);
-    v.push_back(Process(s, a, b));
-  }
+void non_preemptive(std::vector<Process> v, int N) {
+  int total_waiting = 0, total_tat = 0;
 
   // calculate WT, TAT using FCFS
   for (int i = 0; i < N; ++i)
@@ -57,6 +48,38 @@ int main () {
       p.name.c_str(), p.arrival, p.burst, p.start, p.finish, p.tat);
   }
 
-  printf("Average Waiting Time [FCFS] %f\n", (total_waiting*1.0)/N );
-  printf("Average Turn Around Time [FCFS] %f\n", (total_tat*1.0)/N );
+  printf("Average Waiting Time %f\n", (total_waiting*1.0)/N );
+  printf("Average Turn Around Time %f\n", (total_tat*1.0)/N );
+}
+
+bool sortFCFS(Process i, Process j) {
+  return i.arrival < j.arrival;
+}
+
+bool sortSJF(Process i, Process j) {
+  return i.burst < j.burst;
+}
+
+int main () {
+  std::vector<Process> v;
+  int N, a, b;
+  char s[100];
+
+  printf("Enter total number of processes\n");
+  scanf("%d", &N);
+  printf("Enter Process Name, Arrival Time, Burst Time\n");
+  for (int i = 0; i<N; i++) {
+    scanf("%s%d%d", s, &a, &b);
+    v.push_back(Process(s, a, b));
+  }
+
+  printf("FCFS:\n");
+  std::sort(v.begin(), v.end(), sortFCFS);
+  //for (auto k : v) printf("%s %d\n", k.name.c_str(), k.arrival);
+  non_preemptive(v, N); // First Come First Serve
+
+  printf("SJF:\n");
+  std::sort(v.begin(), v.end(), sortSJF);
+  //for (auto k : v) printf("%s %d\n", k.name.c_str(), k.burst);
+  non_preemptive(v, N); // Shortest Job First
 }
