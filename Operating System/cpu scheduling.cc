a17d9b6746e7e012a220d205585e2ce3f3dff5a7
Ashish Gaikwad <ash.gkwd@gmail.com>
2014-10-23 11:45:28 +0530

    Priority scheduling added (non preemptive)

--- Operating System/cpu scheduling.cc
+++ Operating System/cpu scheduling.cc
@@ -11,13 +11,14 @@
 
 class Process {
 public:
-  Process(std::string n, int a, int b) {
+  Process(std::string n, int a, int b, int p = 1) {
     name = n;
     arrival = a;
     burst = b;
+    priority = p;
   }
   std::string name;
-  int arrival, burst, start, finish, tat, waiting;
+  int arrival, burst, priority, start, finish, tat, waiting;
 };
 
 void non_preemptive(std::vector<Process> v, int N) {
@@ -37,15 +38,15 @@ void non_preemptive(std::vector<Process> v, int N) {
     p.waiting = p.start - p.arrival;
   }
 
-  printf("Name\tArrival\tBurst\tStart\tFinish\tTurn Around\n");
+  printf("Name\tArrival\tBurst\tStart\tFinish\tTurn Around\t[Priority]\n");
   for (int i = 0; i < N; ++i)
   {
     Process& p = v[i];
     total_waiting += p.waiting;
     total_tat += p.tat;
 
-    printf("%s \t %d \t %d \t %d \t %d \t %d\n",
-      p.name.c_str(), p.arrival, p.burst, p.start, p.finish, p.tat);
+    printf("%s \t %d \t %d \t %d \t %d \t %d \t\t [%d]\n",
+      p.name.c_str(), p.arrival, p.burst, p.start, p.finish, p.tat, p.priority);
   }
 
   printf("Average Waiting Time %f\n", (total_waiting*1.0)/N );
@@ -60,9 +61,13 @@ bool sortSJF(Process i, Process j) {
   return i.burst < j.burst;
 }
 
+bool sortPriority(Process i, Process j) {
+  return i.priority < j.priority;
+}
+
 int main () {
   std::vector<Process> v;
-  int N, a, b;
+  int N, a, b, c;
   char s[100];
 
   printf("Enter total number of processes\n");
@@ -82,4 +87,18 @@ int main () {
   std::sort(v.begin(), v.end(), sortSJF);
   //for (auto k : v) printf("%s %d\n", k.name.c_str(), k.burst);
   non_preemptive(v, N); // Shortest Job First
+
+  v.clear();
+  printf("Process details for priority scheduling\n");
+  printf("Enter total number of processes\n");
+  scanf("%d", &N);
+  printf("Enter Process Name, Arrival Time, Burst Time\n");
+  for (int i = 0; i<N; i++) {
+    scanf("%s%d%d%d", s, &a, &b, &c);
+    v.push_back(Process(s, a, b, c));
+  }
+
+  printf("Priority:\n");
+  std::sort(v.begin(), v.end(), sortPriority);
+  non_preemptive(v, N);
 }
