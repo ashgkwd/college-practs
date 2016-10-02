7dc537eb5527354f239eb58ec59828b2076e2e94
Ashish Gaikwad <ash.gkwd@gmail.com>
2016-10-02 20:04:01 +0530

    chore(stack): adds push, pop, is_empty, is_full methods

--- Data Structure/Stack.h
+++ Data Structure/Stack.h
@@ -3,8 +3,36 @@
 namespace ashgkwd {
 	class Stack
 	{
+	private:
+		const int MAX = 10;
+		int store[10];
+		int top;
+
 	public:
 		Stack();
 		~Stack();
+
+		bool push(int value) {
+			if(!is_full()) {
+				store[++top] = value;
+				return true;
+			}
+			return false;
+		}
+
+		int pop() {
+			if(!is_empty()) {
+				return store[top--];
+			}
+			return -1;
+		}
+
+		bool is_empty() {
+			return top < 0;
+		}
+
+		bool is_full() {
+			return top >= MAX;
+		}
 	};
 }
