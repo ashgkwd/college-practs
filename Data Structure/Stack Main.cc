bb63a427d8a1b21d3e6c6bca1260fc0fef392d13
Ashish Gaikwad <ash.gkwd@gmail.com>
2016-10-04 23:54:51 +0530

    feat(Stack): adds main program in "Stack Main.cc"

---
+++ Data Structure/Stack Main.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include "Stack.h"
+
+int main(int argc, char const *argv[])
+{
+	ashgkwd::Stack s;
+	s.push(12);
+	int o = s.pop();
+	std::cout << o << std::endl;
+	return 0;
+}
